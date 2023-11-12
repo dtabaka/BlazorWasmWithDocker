c8997c21a5f23b51309ad1790dbb78e7719c00a2
David Tabaka <DavidATabaka@gmail.com>
2023-11-11 20:08:49 -0500

    Added authentication against Azure AD

---
+++ BlazorWasmWithDocker/Services/TokenService.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorWasmWithDocker.Services
+{
+    using System.Threading.Tasks;
+        public interface ITokenService
+    {
+        Task SetTokenAsync(string token);
+        Task<string> GetTokenAsync();
+    }
+
+    public class TokenService : ITokenService
+    {
+        private string token;
+
+        public async Task SetTokenAsync(string newToken)
+        {
+            token = newToken;
+            // You might also want to store the token securely, such as in local storage.
+        }
+
+        public Task<string> GetTokenAsync()
+        {
+            return Task.FromResult(token);
+        }
+    }
+
+}
