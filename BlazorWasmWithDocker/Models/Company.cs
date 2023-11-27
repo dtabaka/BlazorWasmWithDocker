d5838bd4c881fccb0282eca41f86586777377a01
David Tabaka <DavidATabaka@gmail.com>
2023-11-26 21:03:37 -0500

    Added styling, state change notifications, and drawer components

---
+++ BlazorWasmWithDocker/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWasmWithDocker.Models
+{
+    public class Company
+    {
+        public string Symbol { get; set; }
+        public string Cik { get; set; }
+        public string Name { get; set; }
+        public string SubSector { get; set; }
+        public string Sector { get; set; }
+        public string HeadQuarters { get; set; }
+    }
+}
---
+++ BlazorWasmWithDocker/Models/Tomato.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWasmWithDocker.Models
+{
+    public class Tomato
+    {
+        public string DocumentId { get; set; }
+        public string Id { get; set; }
+        public string Variety { get; set; }
+        public string Type { get; set; }
+        public string FruitSet { get; set; }
+        public string DaysToMaturity { get; set; }
+        public string Description { get; set; }
+    }
+}
