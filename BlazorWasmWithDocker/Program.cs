8870412eb69190d1dff1a6f08aedbb737f1beede
David Tabaka <DavidATabaka@gmail.com>
2023-12-18 07:56:06 -0500

    Added png mime type

--- BlazorWasmWithDocker/Program.cs
+++ BlazorWasmWithDocker/Program.cs
@@ -3,9 +3,7 @@ using BlazorWasmWithDocker.Services;
 using BlazorWasmWithDocker.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Authentication.WebAssembly.Msal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,7 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.LoginMode = "redirect";//popup or redict
+    options.ProviderOptions.LoginMode = "redirect";//popup or redirect
     options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("api://d488b2ac-94e8-48b6-b280-32f747ea28ce/.default");
